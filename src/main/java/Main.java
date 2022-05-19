1d9924c086d5f7ce9cf9767abb7c1b89c357d4ba
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2022-05-19 11:02:32 +0200

    add code to move data back to input data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import db_mangament.InsertFileInUserTable;
+import file_management.FileMover;
+import file_management.FolderScanner;
 
+import java.io.File;
 import java.sql.*;
 
 public class Main {
@@ -7,6 +10,31 @@ public class Main {
         String relativePathToFolderContainingData = ".\\ressources\\inputData\\";
         InsertFileInUserTable ifut1 = new InsertFileInUserTable(relativePathToFolderContainingData);
         ifut1.InsertInUserTable();
+
+
+        // Put Data back to input data folder
+//        FolderScanner computedDataFolderScanner = new FolderScanner(".\\ressources\\computedData\\");
+//        FolderScanner problematicDataFolderScanner = new FolderScanner(".\\ressources\\problematicData\\");
+//
+//        for (String fileName:
+//                computedDataFolderScanner.list_dir()) {
+//            File path_to_file = new File(computedDataFolderScanner.getFolder_name() + fileName);
+//            FileMover fileMover = new FileMover(path_to_file.getName(),
+//                    path_to_file.getPath(),
+//                    ".\\ressources\\inputData\\"
+//            );
+//            fileMover.move();
+//        }
+//        for (String fileName:
+//                problematicDataFolderScanner.list_dir()) {
+//            File path_to_file = new File(problematicDataFolderScanner.getFolder_name() + fileName);
+//            FileMover fileMover = new FileMover(path_to_file.getName(),
+//                    path_to_file.getPath(),
+//                    ".\\ressources\\inputData\\"
+//            );
+//            fileMover.move();
+//        }
+
 
     }
 }
