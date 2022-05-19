505de0f7a2bc2d1196d11c6512129bd1f677c9b0
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2022-05-19 10:42:57 +0200

    replace db_connect.java by Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,12 @@
-package db_mangament;
+import db_mangament.InsertFileInUserTable;
 
-import file_management.FileMover;
-import file_management.FolderScanner;
-import utils.StringToUnixTmstp;
-import utils.TimeStampFinder;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.sql.*;
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class db_connect {
-    public static void main(String[] args) throws SQLException {
-        /* Test connexion */
-
-//        DatabaseOperations a = new DatabaseOperations("localhost:3306", "users_db", "root", "root");
-//        DatabaseMetaData rst = a.displayElementsInDB("users_db");
-
-
-        /*
-        Test the function insert on a file which is  well formatted
-        It is then placed in computed data folder
-        */
-
-        InsertFileInUserTable ifut1 = new InsertFileInUserTable(".\\ressources\\inputData\\");
+public class Main {
+    public static void main(String[] args){
+        String relativePathToFolderContainingData = ".\\ressources\\inputData\\";
+        InsertFileInUserTable ifut1 = new InsertFileInUserTable(relativePathToFolderContainingData);
         ifut1.InsertInUserTable();
-
-
-        /*
-        Test the function insert on a file which is not well formatted
-        It is then placed in problematic data folder
-        */
-
-//        InsertFileInUserTable ifut2 = new InsertFileInUserTable(".\\ressources\\inputData\\user_20210606220136.csv");
-//        ifut2.InsertInUserTable();
-
-        /* Test folder scanner class */
-
-//        FolderScanner fs = new FolderScanner(".\\ressources\\inputData\\");
-//        String test[] = fs.list_dir();
-//        Arrays.stream(test).toList().forEach(System.out::println);
-//        System.out.println(test[0]);
 
     }
 }
